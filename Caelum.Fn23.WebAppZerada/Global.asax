392206848d5c155be1a311ccfced1139ce62b42a
dpcosta <dpcosta@gmail.com>
2018-04-09 18:47:52 -0300

    Segunda solução dinâmica usando HttpApplication.

---
+++ Caelum.Fn23.WebAppZerada/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Caelum.Fn23.WebAppZerada.Global" Language="C#" %>
---
+++ Caelum.Fn23.WebAppZerada/Global.asax.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Caelum.Fn23.WebAppZerada
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            //esse evento é chamado qdo a requisição começará a ser tratada
+            var lista = new List<string>
+            {
+                "Highway to Hell",
+                "If You Want Blood",
+                "TNT",
+                "Thunderstruck",
+            };
+            var artigos = "";
+            foreach (var post in lista)
+            {
+                artigos += $"<li>{post}</li>";
+            }
+            //mas como disponibilizar essa lista no navegador?
+            HttpContext.Current.Response.Write(artigos);
+            HttpContext.Current.Response.End();
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
